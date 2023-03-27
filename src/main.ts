eb0887acd731cdd420607c480ebff016f692efd3
Jisu Sim <rlj1202@gmail.com>
2023-03-27 15:45:10 +0900

    Add class-validator and class-transformer and apply it globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Pipes
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  // Interceptors
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   await app.listen(3000);
 }
 bootstrap();
