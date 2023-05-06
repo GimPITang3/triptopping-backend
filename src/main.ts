ede2156456352b6fd82e242d377ea0801f263ae5
qilip <qilip@qilip.io>
2023-05-07 03:38:34 +0900

    Fix: Nestjs port setting

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory, Reflector } from '@nestjs/core';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = 3001;
+  const port = process.env.PORT || 3001;
 
   // Pipes
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
