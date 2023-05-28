24663195e7f5b9bb00fe893429d8464067f1a71b
jinhyeongju <jujh97@hanmail.net>
2023-05-28 21:12:41 +0900

    isNumber 삭제

--- src/pagination/pagination-options.dto.ts
+++ src/pagination/pagination-options.dto.ts
@@ -2,11 +2,9 @@ import { Type } from 'class-transformer';
 import { IsNumber } from 'class-validator';
 
 export class PaginationOptionsDto {
-  @IsNumber()
   @Type(() => Number)
   skip: number;
 
-  @IsNumber()
   @Type(() => Number)
   limit: number;
 }
