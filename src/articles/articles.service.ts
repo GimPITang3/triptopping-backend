3ed58f009c9721a1d64860a404a905a96287707d
Jisu Sim <rlj1202@gmail.com>
2023-05-30 17:37:24 +0900

    Fix(ArticlesModule): Update cover image url

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -111,6 +111,7 @@ export class ArticlesService {
         {
           ...{ title: dto.title },
           ...{ content: dto.content },
+          ...{ coverImageUrl: dto.coverImageUrl },
           ...{ plan: plan },
         },
         { returnOriginal: false },
